be3c7fd2aa807cdc2b293a8545f1015b3477bf73
Zacharybroeg <Zacharybroeg@u.boisestate.edu>
2018-11-13 12:49:22 -0700

    Backup to Git Before the VR stuff

---
+++ VRGAME/VRGAME/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -0,0 +1,94 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class AICharacterControl : MonoBehaviour
+    {
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
+        public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+        public Transform target;    
+        
+        public GameObject[] players;          
+        
+        int i;      
+                      
+
+
+        private void Start()
+        {
+            // get the components on the object we need ( should not be null due to require component so no need to check )
+            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+            
+	        agent.updateRotation = false;
+	        agent.updatePosition = true;
+        }
+
+
+        private void Update()
+        {
+            players = GameObject.FindGameObjectsWithTag("Player");
+            if (players.Length > 0) {
+                target = players[i].transform;
+               // Debug.Log("Player " + target);
+               // Debug.Log(players.Length);
+            }
+             agent.isStopped = false;
+           
+           GameObject closest = null;
+                    float distance = Mathf.Infinity;
+                    Vector3 postiion = players[i].transform.position;
+
+            foreach ( GameObject player in players) {
+                
+                Vector3 diff = player.transform.position - transform.position;
+                float curDistance = diff.sqrMagnitude;
+                if(curDistance < distance) {
+                    closest = player;
+                    distance = curDistance;
+                    if((closest.transform.position - transform.position).magnitude <= 25.0f) {
+                        agent.SetDestination(closest.transform.position);
+                       
+                    } else  {
+                        agent.isStopped = true;
+                    }
+                    
+                     
+                     
+                     
+                if (agent.remainingDistance > agent.stoppingDistance) {
+                character.Move(agent.desiredVelocity, false, false);
+                 } else{
+                character.Move(Vector3.zero, false, false);
+                }
+            }
+                }
+                // if (target != null) 
+                // {
+                    
+                //     if((player.transform.position - transform.position).magnitude <= 25.0f) 
+                //     {
+                //         agent.SetDestination(target.position);
+                //          Debug.Log(agent);
+        
+                //     }
+                    
+                // }  
+                
+               
+            }
+               
+
+           
+        
+
+      
+        public void SetTarget(Transform target)
+        {
+            this.target = target;
+        }
+    }
+}
